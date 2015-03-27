f966cde8370cde14401ac88b6cf446dddb185654
Luke <Luke-Farmlogs@lukes-mbp.attlocal.net>
2015-03-26 23:14:25 -0400

    commented out line, added another

--- hello_word/hello_world.py
+++ hello_word/hello_world.py
@@ -1,3 +1,4 @@
 print "hellow world"
 print "i just updated this file"
-print "i just updated from github"
+#print "i just updated from github"
+print "updated from computer, comment out line before"
